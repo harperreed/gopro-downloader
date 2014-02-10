4f54aa09371b2e17a60dff2b67d4529e653cc6a1
Konrad Iturbe <github@chernowii.com>
2014-02-10 16:07:32 +0100

    Create wifi.py

---
+++ GoProTimeLapse/wifi.py
@@ -0,0 +1,6 @@
+# GoPro Timelapse WiFi Raspberry Pi Python by Konrad Iturbe (@konradit in github)
+import time
+import urllib2
+while True:
+time.sleep(X) # Replace X by the intervalof the timelapse
+urllib2.urlopen("http://10.5.5.9/bacpac/SH?t=password&t=%02")
