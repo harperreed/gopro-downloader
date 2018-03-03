956b3d88a034f18ce614fb867c61667bfe333e8b
Harper Reed <harper@nata2.org>
2018-03-03 11:13:38 -0600

    tweaked a bit and changed how logic worked. Also moved to using requests instead of urllib

--- move_gopro_images.py
+++ move_gopro_images.py
@@ -1,38 +1,55 @@
 from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
  
 import urllib2
+import requests
 from bs4 import BeautifulSoup
 import urlparse
 import shutil
 
 import re, os
+
+
+def delete_image(delete_filename):
+    delete_url = "http://10.5.5.9/gp/gpControl/command/storage/delete?p="+delete_filename
+    #print(delete_url)
+    print('Deleting:',delete_filename)
+    r = requests.get(delete_url)
+    if (r.status_code==200):
+        print("Successfully deleted", delete_filename)
+    else:
+        print("Error occurred deleting", delete_filename)
+
+
  
 base_url = "http://10.5.5.9:8080/videos/DCIM/100GOPRO/" #Where XXX, change it by the directory you want (for instance 100GOPRO)
-content = urllib2.urlopen(base_url).read()
-soup = BeautifulSoup(content)
+content = requests.get(base_url).text
+soup = BeautifulSoup(content, "lxml")
  
 media_re = re.compile(r'(jpg|jpeg|mp4)$', re.IGNORECASE)
-home_image_dir = os.path.join(os.path.expanduser("~"),'Pictures', 'gopro')
-print("Home directory:",home_image_dir)
+image_download_dir = "./gopro" #os.path.join(os.path.expanduser("~"),'Pictures', 'gopro')
+print("Download directory:",image_download_dir)
  
 try:
-    os.makedirs(home_image_dir)
-    print("Created:",home_image_dir)
+    os.makedirs(image_download_dir)
+    print("Created:",image_download_dir)
 except (OSError):
-    print('Already exists:',home_image_dir)
+    print('Already exists:',image_download_dir)
  
 for a in soup.findAll('a', attrs={'href': media_re}):
     print("Found the URL:", a['href'])
     req = urllib2.urlopen(urlparse.urljoin(base_url, a['href']))
-    file_name = os.path.join(home_image_dir, a['href'].split('/')[-1])
+    file_name = os.path.join(image_download_dir, a['href'].split('/')[-1])
     if(os.path.isfile(file_name)):
         print('Already exists:',file_name)
-        #delete file 
         delete_filename = a['href'].replace("/videos/DCIM", "")
-        delete_url = "http://10.5.5.9/gp/gpControl/command/storage/delete?p="+delete_filename
-        print('Deleting:',delete_filename)
-        content = urllib2.urlopen(delete_url).read()
+        delete_image(delete_filename)
+        #delete file 
+        
     else:
         print("Downloading to:", file_name)
         with open(file_name, 'wb') as fp:
             shutil.copyfileobj(req, fp)
+        if(os.path.isfile(file_name)):
+            print("Downloaded", file_name)
+            delete_filename = a['href'].replace("/videos/DCIM", "")
+            delete_image(delete_filename)
